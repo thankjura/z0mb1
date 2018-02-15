ad1eda1149dcd27e4a941dea8012d4749e99bf4b
thankjura <me@slie.ru>
2018-02-16 01:50:20 +0300

    WIP jump

--- scripts/enemy/base_enemy.gd
+++ scripts/enemy/base_enemy.gd
@@ -1,20 +1,15 @@
 extends KinematicBody2D
 
 const constants = preload("res://scripts/constants.gd")
-const MOVEMENT_SPEED = 50
-const ATTACK_DISTANCE = 300
-const ATTACK_FOLLOW_TIMEOUT = 5
-const ATTACK_DAMAGE = 30
 const GRAVITY = Vector2(0, 2000)
 const MAX_FALL_SPEED = 800
-
-var ATTACK_DISTANCE_SQUARED = pow(ATTACK_DISTANCE, 2)
+const FLOOR_NORMAL = Vector2(0, -1)
 
 onready var player = get_parent().get_node("player")
+
 export(int, 10, 1000) var health = 100
-var attacking = false
-var attacking_timeout = 0
-var active = true
+
+var dead = false
 var velocity = Vector2()
 var recoil = Vector2()
 
@@ -31,63 +26,28 @@ func _get_player_direction():
 func is_back():
     return $base.scale.x == -1
 
-func _check_attack():
-    if attacking:
-        return
-    var new_state
-    if has_node("visibility_notifier"):
-        new_state = $visibility_notifier.is_on_screen() and get_global_position().distance_squared_to(player.global_position) < ATTACK_DISTANCE_SQUARED
-    else:
-        new_state = get_global_position().distance_squared_to(player.global_position) < ATTACK_DISTANCE_SQUARED
-
-    var b = is_back()
-    if new_state:
-        if _get_player_direction() == 1:
-            if b:
-                new_state = false
-        else:
-            if not b:
-                new_state = false
-
-    if new_state and attacking_timeout <= 0:
-        attacking = true
-        attacking_timeout = ATTACK_FOLLOW_TIMEOUT
-
-
-func _deactivate():
-    set_collision_mask(constants.GROUND_LAYER)
-    active = false
-
 func die():
-    _deactivate()
+    dead = true
+    set_collision_mask(constants.GROUND_LAYER)
 
 func damage(d, v):
     recoil += v / 20
-    if active:
+    if not dead:
         health -= d
 
 func _process(delta):
-    if not active:
+    if dead:
         return
 
     if health <= 0:
         die()
-
-    _check_attack()
-
-    if attacking and attacking_timeout > 0:
-        attacking_timeout -= delta
-        if attacking_timeout <= 0:
-            attacking = false
 
 func _physics_process(delta):
     velocity += GRAVITY * delta
     if velocity.y > MAX_FALL_SPEED:
         velocity.y = MAX_FALL_SPEED
 
-    if not active:
-        velocity = velocity.linear_interpolate(Vector2(), 20*delta)
-    velocity = move_and_slide(velocity + recoil)
+    velocity = move_and_slide(velocity + recoil, FLOOR_NORMAL)
     velocity -= recoil
     var new_recoil = recoil.linear_interpolate(Vector2(), 20*delta)
     if sign(recoil.x) != sign(new_recoil.x):
--- scripts/enemy/solder.gd
+++ scripts/enemy/solder.gd
@@ -2,30 +2,40 @@ extends "res://scripts/enemy/base_enemy.gd"
 
 const ATTACK_DISTANCE = 1000
 const ATTACK_DAMAGE = 30
+const BACKSLIDE_DISTANCE = 50
+
+const MAX_SPEED = 100
+const MAX_RUN_SPEED = 150
+const MAX_JUMP = 400
+const ACCELERATION = 20
+const AIM_TIME = 3
 
 const DEFAULT_VECTOR = Vector2(0, -1)
 
 const BULLET = preload("res://scenes/enemy/bullets/enemy_pistol_bullet.tscn")
 const BULLET_SPEED = 600
 
-const MAX_SPEED = 100
-const ACCELERATION = 20
-const AIM_TIME = 3
-
-const DIRECTION_RIGHT = 1
-const DIRECTION_LEFT = -1
-
 const BODY_STRENGTH = 120
 const HEAD_STRENGTH = 100
 
+enum STATE {
+    IDLE,
+    WALK,
+    RELOCATE,
+    JUMP
+}
+
 onready var bullet_spawn = get_node("base/base/body/sholder_l/forearm_l/hand_l_pistol/pistol/bullet_spawn")
 onready var world = get_tree().get_root().get_node("world")
+onready var ray_bottom = get_node("base/base/ray_cast_bottom")
+onready var ray_height = get_node("base/base/ray_cast_height")
 
 export(int, "left", "stay", "right") var direction = 0 setget _set_init_direction
 
-# timeouts
-var aim_timeout = 0
-var idle_timeout = 0
+export(STATE) var current_state
+var next_state
+var switch_state_timeout = 0
+export(STATE) var block_behavior = STATE.IDLE
 
 func _ready():
     $base/base/body/body_area.set_collision_layer(constants.ENEMY_DAMAGE_LAYER)
@@ -39,6 +49,8 @@ func _ready():
     if direction != $base.scale.x:
         direction = -direction
         _set_direction(-direction)
+        
+    next_state = STATE.WALK
 
 func _body_hit(obj):
     if obj.has_method("damage"):
@@ -61,9 +73,9 @@ func _head_hit(obj):
 func _set_direction(new_direction):
     if direction != new_direction:
         direction = new_direction
-        if direction == DIRECTION_LEFT:
+        if direction == -1:
             $base.set_scale(Vector2(-1,1))
-        elif direction == DIRECTION_RIGHT:
+        elif direction == 1:
             $base.set_scale(Vector2(1,1))
 
 func _set_init_direction(new_direction):
@@ -86,49 +98,55 @@ func _aim(delta):
     var v = (player.get_node("aim").global_position -  bullet_spawn.global_position).normalized()
     var a = $anim.aim(rad2deg(DEFAULT_VECTOR.angle_to(v)), delta)
     if a > 0 and a < 180:
-        _set_direction(DIRECTION_RIGHT)
+        _set_direction(1)
     elif a > -180 and a < 0:
-        _set_direction(DIRECTION_LEFT)
+        _set_direction(-1)
 
 func change_direction(t, new_direction):
+    current_state = STATE.IDLE
+    switch_state_timeout = t
     _set_direction(new_direction)
-    idle_timeout = t
 
 func die():
-    _deactivate()
+    .die()
     $base/base/body/body_area.disconnect("body_entered", self, "_body_hit")
     $base/base/body/head/head_area.disconnect("body_entered", self, "_head_hit")
     $anim.die()
 
 func _process(delta):
-    if not active:
+    if dead:
         return
-
-    if aim_timeout > 0:
-        aim_timeout -= delta
-        if attacking:
-            if aim_timeout <= 0:
-                _fire()
-            else:
-                _aim(delta)
-    elif attacking:
-        aim_timeout = AIM_TIME
+        
+    if current_state != next_state:
+        switch_state_timeout -= delta
+        if switch_state_timeout < 0:
+            current_state = next_state
+        
 
 func _physics_process(delta):
-    if not active:
+    if dead:
         return
 
-    if attacking or aim_timeout > 0:
-        if abs(velocity.x) > 0:
-            velocity.x = lerp(velocity.x, 0, ACCELERATION*delta)
-    elif idle_timeout > 0:
-        idle_timeout -= delta
-        if abs(velocity.x) > 0:
-            velocity.x = lerp(velocity.x, 0, ACCELERATION*delta)
-    else:
+    if ray_bottom.is_colliding():        
+        if block_behavior == STATE.IDLE:
+            next_state = STATE.IDLE            
+        elif block_behavior == STATE.JUMP:
+            var p = ray_height.get_collision_point()
+            var h = ray_height.cast_to.y - (p.y - ray_height.global_position.y)
+            if h < MAX_JUMP:
+                switch_state_timeout = 2
+                current_state = STATE.IDLE
+                next_state = STATE.JUMP
+            else:
+                change_direction(1, -direction)
+        
+
+    if current_state == STATE.IDLE:
+        velocity.x = lerp(velocity.x, 0, ACCELERATION*delta)
+    elif current_state == STATE.WALK:
         velocity.x = lerp(velocity.x, MAX_SPEED * direction, ACCELERATION*delta)
 
-    if abs(velocity.x) > 5:
+    if velocity.x:
         $anim.walk(abs(velocity.x)*delta*1.2)
     else:
         $anim.idle()
